a8be98bafaf49a764405034c761b5e55d157eab7
Aaditya Dhote <AadityaDhote@outlook.com>
2022-10-01 15:55:26 +0530

    edited

---
+++ index.js
@@ -0,0 +1,32 @@
+const dob = document.querySelector("#dob");
+const num = document.querySelector("#num");
+const check = document.querySelector("#check");
+const result=document.querySelector("#result");
+
+function equalValues(sumD,num){
+    if(sumD%num=== 0){result.innerText="Your Birthday is lucky";}
+    else{result.innerText="Your Birthday is unlucky";}
+    }
+
+function sumOfDob(dateOfBirth){
+    dateOfBirth=dateOfBirth.replaceAll("-","");
+ let sumD=0;
+for(let i=0 ; i<dateOfBirth.length ; i++)
+{sumD = sumD + Number(dateOfBirth.charAt(i));}
+return sumD;
+}
+
+
+
+
+
+function isBirthDayLucky(){
+const dateOfBirth=dob.value;
+const sumD=sumOfDob(dateOfBirth);
+equalValues(sumD,num.value);
+
+}
+
+
+check.addEventListener("click", isBirthDayLucky)
+
